86cf356faa344152158150470c13545f0644926e
CNUB <72559571+CNUB@users.noreply.github.com>
2025-07-21 01:10:17 +0900

    Add files via upload

---
+++ financescope/src/test/java/com/financescope/financescope/FinancescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financescope.financescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
